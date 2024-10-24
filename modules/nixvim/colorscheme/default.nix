965d56e1654ee5e92ffd3dfab3dbe90324193e57
Luc Lebel <luc.lebel@achievers.com>
2024-10-24 14:29:49 -0400

    remove nord theme post stuff

--- modules/nixvim/colorscheme/default.nix
+++ modules/nixvim/colorscheme/default.nix
@@ -2,17 +2,6 @@
 {
   extraConfigLuaPost = # lua
     ''
-      do
-        -- The NixVim Nord colorscheme doesn't assign a pleasing color to the window
-        -- separator by default. Here we override it with a softer color.
-        local colors = require("nord.colors")
-        vim.api.nvim_set_hl(0,  "WinSeparator", {
-          fg = colors.nord2_gui,
-        })
-        vim.api.nvim_set_hl(0,  "LspInlayHint", {
-          fg = colors.nord3_gui,
-        })
-      end
     '';
 
 
