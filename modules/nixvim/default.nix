700c243790fff369139bca70282caccd17a13993
Luc Lebel <luc.lebel@achievers.com>
2024-10-24 11:57:09 -0400

    OG backup folder?

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -118,6 +118,18 @@
 
 	extraConfigLua = # lua
     ''
+			HOME = os.getenv("HOME")
+			BACKUPDIR = HOME .. "/.vim/backup"
 
+			if vim.fn.isDirectory(BACKUPDIR) == 0 then
+				vim.fn.mkdir(BACKUPDIR, "p", "0o700")
+			end
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				callback = function()
+					local extention = "~" .. vim.fn.strftime("~%Y-%m-%d_%H-%M-%S~")
+					vim.o.backupext = extension
+				end,
+			})
     '';
 }
