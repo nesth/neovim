916f2cb02f70e5f89b7d5102dbdad9281d167595
Luc Lebel <luc.lebel@achievers.com>
2024-10-24 15:02:05 -0400

    cleanup

--- modules/nixvim/colorscheme/default.nix
+++ modules/nixvim/colorscheme/default.nix
@@ -1,18 +1,11 @@
 { helpers, ... }:
 {
-  extraConfigLuaPost = # lua
-    ''
-    '';
-
+  extraConfigLuaPost = "" # lua
 
   colorschemes.onedark = {
     enable = true;
     settings = {
 			style = "warmer";
-      borders = true;
-      contrast = false;
-      disable_background = false;
-      enable_sidebar_background = true;
     };
   };
 }
