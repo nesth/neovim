593c922eb4a346199b4871514a73ed4b360508e2
Luc Lebel <luc.lebel@achievers.com>
2024-10-29 14:12:23 -0400

    Revert "remove gemini"
    This reverts commit fd9a125457359a9c2e712ede50e0159e97b3bc3a.

---
+++ modules/nixvim/avante/default.nix
@@ -0,0 +1,14 @@
+{
+  plugins = {
+    avante-nvim = {
+      enable = true;
+      provider = "gemini";
+      auto_suggestion_provider = "gemini";
+      settings = {
+        gemini = {
+          enable = true;
+        };
+      };
+    };
+  };
+}
