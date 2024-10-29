b857890638aa8e480f531217c7a75ca804c3722e
Luc Lebel <luc.lebel@achievers.com>
2024-10-29 13:28:03 -0400

    add gemini

---
+++ modules/nixvim/avante/default.nix
@@ -0,0 +1,12 @@
+{
+  plugins.avante = {
+    enable = true;
+    provider = "gemini";
+    auto_suggestion_provider = "gemini";
+    settings = {
+      gemini = {
+        enable = true;
+      };
+    };
+  };
+}
