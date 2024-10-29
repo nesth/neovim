443008873a133df017e9a7c8a801fe60232bfc76
Luc Lebel <luc.lebel@achievers.com>
2024-10-29 13:42:59 -0400

    gemini attempt 2

--- modules/nixvim/avante/default.nix
+++ modules/nixvim/avante/default.nix
@@ -1,11 +1,13 @@
 {
-  plugins.avante = {
-    enable = true;
-    provider = "gemini";
-    auto_suggestion_provider = "gemini";
-    settings = {
-      gemini = {
-        enable = true;
+  plugins = {
+    avante-nvim = {
+      enable = true;
+      provider = "gemini";
+      auto_suggestion_provider = "gemini";
+      settings = {
+        gemini = {
+          enable = true;
+        };
       };
     };
   };
