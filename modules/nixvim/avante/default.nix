cb7afd51c2d3dc446b70e39153f31f8d11fd358f
Luc Lebel <luc.lebel@achievers.com>
2024-12-19 14:32:12 -0500

    flake update

--- modules/nixvim/avante/default.nix
+++ modules/nixvim/avante/default.nix
@@ -3,6 +3,11 @@
     enable = true;
     settings = {
       provider = "gemini";
+      auto_suggestions_provider = "gemini";
+      behaviour = {
+        auto_suggestions = true;
+
+      };
     };
   };
 }
